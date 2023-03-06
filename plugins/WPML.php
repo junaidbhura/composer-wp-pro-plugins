d5d1fe72168eb7f664162493df6b67fc982f9eb8
Chauncey McAskill <chauncey@mcaskill.ca>
2023-03-06 18:14:45 -0500

    Fix indentation style
    Should be tabs instead of spaces.

--- plugins/WPML.php
+++ plugins/WPML.php
@@ -14,15 +14,15 @@ use UnexpectedValueException;
  */
 class Wpml extends AbstractPlugin {
 
-    /**
-     * Wpml constructor.
-     *
-     * @param string $version
-     * @param string $slug
-     */
-    public function __construct( $version = '', $slug = 'wpml-sitepress-multilingual-cms' ) {
-        parent::__construct( $version, $slug );
-    }
+	/**
+	 * Wpml constructor.
+	 *
+	 * @param string $version
+	 * @param string $slug
+	 */
+	public function __construct( $version = '', $slug = 'wpml-sitepress-multilingual-cms' ) {
+		parent::__construct( $version, $slug );
+	}
 
 	/**
 	 * Get the download URL for this plugin.
@@ -30,34 +30,34 @@ class Wpml extends AbstractPlugin {
 	 * @return string
 	 */
 	public function getDownloadUrl() {
-        $packages = array(
-            'wpml-acfml'                       => 1097589,
-            'wpml-all-import'                  => 720221,
-            'wpml-buddypress-multilingual'     => 2216259,
-            'wpml-cms-nav'                     => 6096,
-            'wpml-contact-form-7-multilingual' => 3156699,
-            'wpml-gravityforms-multilingual'   => 8882,
-            'wpml-mailchimp-for-wp'            => 1442229,
-            'wpml-media-translation'           => 7474,
-            'wpml-ninja-forms'                 => 5342487,
-            'wpml-sitepress-multilingual-cms'  => 6088,
-            'wpml-sticky-links'                => 6090,
-            'wpml-string-translation'          => 6092,
-            'wpml-translation-management'      => 6094,
-            'wpml-types'                       => 1385906,
-            'wpml-woocommerce-multilingual'    => 637370,
-            'wpml-wp-seo-multilingual'         => 3566177,
-            'wpml-wpforms'                     => 5368995,
-        );
+		$packages = array(
+			'wpml-acfml'                       => 1097589,
+			'wpml-all-import'                  => 720221,
+			'wpml-buddypress-multilingual'     => 2216259,
+			'wpml-cms-nav'                     => 6096,
+			'wpml-contact-form-7-multilingual' => 3156699,
+			'wpml-gravityforms-multilingual'   => 8882,
+			'wpml-mailchimp-for-wp'            => 1442229,
+			'wpml-media-translation'           => 7474,
+			'wpml-ninja-forms'                 => 5342487,
+			'wpml-sitepress-multilingual-cms'  => 6088,
+			'wpml-sticky-links'                => 6090,
+			'wpml-string-translation'          => 6092,
+			'wpml-translation-management'      => 6094,
+			'wpml-types'                       => 1385906,
+			'wpml-woocommerce-multilingual'    => 637370,
+			'wpml-wp-seo-multilingual'         => 3566177,
+			'wpml-wpforms'                     => 5368995,
+		);
 
-        if ( ! array_key_exists( $this->slug, $packages ) ) {
-            throw new UnexpectedValueException( sprintf(
-                'Could not find a matching package for %s. Check the package spelling and that the package is supported',
-                'junaidbhura/' . $this->slug
-            ) );
-        }
+		if ( ! array_key_exists( $this->slug, $packages ) ) {
+			throw new UnexpectedValueException( sprintf(
+				'Could not find a matching package for %s. Check the package spelling and that the package is supported',
+				'junaidbhura/' . $this->slug
+			) );
+		}
 
-        return 'https://wpml.org/?download=' . $packages[ $this->slug ] . '&user_id=' . getenv( 'WPML_USER_ID' ) . '&subscription_key=' . getenv( 'WPML_KEY' ) . '&version=' . $this->version;
+		return 'https://wpml.org/?download=' . $packages[ $this->slug ] . '&user_id=' . getenv( 'WPML_USER_ID' ) . '&subscription_key=' . getenv( 'WPML_KEY' ) . '&version=' . $this->version;
 	}
 
 }
