565ac02fc31e58d191a386e05af5b9f056f212dd
Chauncey McAskill <chauncey@mcaskill.ca>
2023-03-08 17:37:19 -0500

    Improve Gravity Forms download resolution
    Use Semver to check main and latest versions.
    
    Prioritize main version (`download_url`) over latest version (`download_url_latest`) since most people will only specify `MAJOR.MINOR.PATCH` as opposed to Gravity Forms `PARADIGM.MAJOR.MINOR.PATCH`. This also prevents "contamination" of a cache key for a "main version" with the contents of the "latest version".

--- src/Plugins/GravityForms.php
+++ src/Plugins/GravityForms.php
@@ -29,36 +29,84 @@ class GravityForms extends AbstractPlugin {
 	/**
 	 * Get the download URL for this plugin.
 	 *
+	 * @throws UnexpectedValueException If the response is invalid.
 	 * @return string
 	 */
 	public function getDownloadUrl() {
 		$http     = new Http();
-		$response = unserialize( $http->post( 'https://gravityapi.com/wp-content/plugins/gravitymanager/api.php?op=get_plugin&slug=' . $this->slug . '&key=' . getenv( 'GRAVITY_FORMS_KEY' ) ) );
+		$response = unserialize( $http->get( 'https://gravityapi.com/wp-content/plugins/gravitymanager/api.php', array(
+			'op'   => 'get_plugin',
+			'slug' => $this->slug,
+			'key'  => getenv( 'GRAVITY_FORMS_KEY' ),
+		) ) );
 
-		if ( empty( $response['download_url_latest'] ) || ! is_string( $response['download_url_latest'] ) ) {
+		if ( ! is_array( $response ) ) {
 			throw new UnexpectedValueException( sprintf(
-				'Expected a valid download URL for package %s',
+				'Expected a serialized object from API for package %s',
 				'junaidbhura/' . $this->slug
 			) );
 		}
 
-		if ( empty( $response['version_latest'] ) || ! is_scalar( $response['version_latest'] ) ) {
+		$candidates = array(
+			array( 'version', 'download_url' ),
+			array( 'version_latest', 'download_url_latest' ),
+		);
+		foreach ( $candidates as $candidate ) {
+			list( $version_key, $download_key ) = $candidate;
+
+			try {
+				return $this->extractDownloadUrl( $response, $version_key, $download_key );
+			} catch ( UnexpectedValueException $e ) {
+				// throw the last exception after the loop
+			}
+		}
+
+		throw $e;
+	}
+
+	/**
+	 * @param  array<string, mixed> $response     The EDD API response.
+	 * @param  string               $version_key  The API field key that holds the version.
+	 * @param  string               $download_key The API field key that holds the download URL.
+	 * @throws UnexpectedValueException If the response is not OK, invalid, or malformed.
+	 * @return string
+	 */
+	protected function extractDownloadUrl( array $response, $version_key, $download_key ) {
+		$version      = array_key_exists( $version_key, $response ) ? $response[ $version_key ] : null;
+		$download_url = array_key_exists( $download_key, $response ) ? $response[ $download_key ] : null;
+
+		if ( false === $version ) {
+			// If FALSE, the package does not exist / is not available
 			throw new UnexpectedValueException( sprintf(
-				'Expected a valid download version number for package %s',
+				'Could not find a matching package for %s. Check the package spelling and that the package is supported',
 				'junaidbhura/' . $this->slug
 			) );
 		}
 
-		if ( ! Semver::satisfies( $response['version_latest'], $this->version ) ) {
+		if ( empty( $download_url ) || ! is_string( $download_url ) ) {
 			throw new UnexpectedValueException( sprintf(
-				'Expected download version (%s) to match installed version (%s) of package %s',
-				$response['version_latest'],
+				'Expected a valid download URL from API for package %s',
+				'junaidbhura/' . $this->slug
+			) );
+		}
+
+		if ( empty( $version ) || ! is_scalar( $version ) ) {
+			throw new UnexpectedValueException( sprintf(
+				'Expected a valid download version number from API for package %s.',
+				'junaidbhura/' . $this->slug
+			) );
+		}
+
+		if ( ! Semver::satisfies( $version, $this->version ) ) {
+			throw new UnexpectedValueException( sprintf(
+				'Expected download version from API (%s) to match installed version (%s) of package %s.',
+				$version,
 				$this->version,
 				'junaidbhura/' . $this->slug
 			) );
 		}
 
-		return str_replace( 'http://', 'https://', $response['download_url_latest'] );
+		return str_replace( 'http://', 'https://', $download_url );
 	}
 
 }
